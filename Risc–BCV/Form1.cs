ec84a97dcd934823a33edad0a02012c5d6fec3ef
Sandru Gabriel <gabi200611@yahoo.com>
2023-07-05 19:52:20 +0300

    done

--- Risc–BCV/Form1.cs
+++ Risc–BCV/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Risc_BCV.Panels;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,29 @@ namespace Risc_BCV
         public Form1()
         {
             InitializeComponent();
+
+            this.Controls.Add(new PnlStart(this));
+
+
         }
+
+        public void removePnl(string pnl)
+        {
+
+            Control control = null;
+
+            foreach (Control c in this.Controls)
+            {
+
+                if (c.Name.Equals(pnl))
+                {
+                    control = c;
+                }
+
+            }
+
+            this.Controls.Remove(control);
+        }
+
     }
 }
--- Risc–BCV/Mockups/MockupStart.Designer.cs
+++ Risc–BCV/Mockups/MockupStart.Designer.cs
@@ -31,10 +31,10 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.lblTitlu = new System.Windows.Forms.Label();
             this.lblEmail = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtParola = new System.Windows.Forms.TextBox();
+            this.lblParola = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -67,52 +67,52 @@
             this.lblEmail.TabIndex = 2;
             this.lblEmail.Text = "Email";
             // 
-            // textBox1
+            // txtEmail
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(208, 223);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(325, 38);
-            this.textBox1.TabIndex = 3;
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(208, 223);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(325, 38);
+            this.txtEmail.TabIndex = 3;
             // 
-            // textBox2
+            // txtParola
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(208, 371);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(325, 38);
-            this.textBox2.TabIndex = 5;
+            this.txtParola.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtParola.Location = new System.Drawing.Point(208, 371);
+            this.txtParola.Name = "txtParola";
+            this.txtParola.Size = new System.Drawing.Size(325, 38);
+            this.txtParola.TabIndex = 5;
             // 
-            // label1
+            // lblParola
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 15.8F);
-            this.label1.Location = new System.Drawing.Point(202, 333);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 35);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Parola";
+            this.lblParola.AutoSize = true;
+            this.lblParola.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 15.8F);
+            this.lblParola.Location = new System.Drawing.Point(202, 333);
+            this.lblParola.Name = "lblParola";
+            this.lblParola.Size = new System.Drawing.Size(91, 35);
+            this.lblParola.TabIndex = 4;
+            this.lblParola.Text = "Parola";
             // 
-            // button1
+            // btnLogin
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.SystemColors.Control;
-            this.button1.Location = new System.Drawing.Point(260, 485);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(240, 62);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Login";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnLogin.Location = new System.Drawing.Point(260, 485);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(240, 62);
+            this.btnLogin.TabIndex = 6;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
             // 
             // MockupStart
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(789, 584);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtParola);
+            this.Controls.Add(this.lblParola);
+            this.Controls.Add(this.txtEmail);
             this.Controls.Add(this.lblEmail);
             this.Controls.Add(this.lblTitlu);
             this.Controls.Add(this.pictureBox1);
@@ -129,9 +129,9 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label lblTitlu;
         private System.Windows.Forms.Label lblEmail;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtParola;
+        private System.Windows.Forms.Label lblParola;
+        private System.Windows.Forms.Button btnLogin;
     }
 }
